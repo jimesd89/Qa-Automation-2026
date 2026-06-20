03dad72ddd80257a7f0333a20ed621e9234d78e7
MVReyna <vickyreyna79@gmail.com>
2026-06-20 14:21:36 -0300

    Automatización desde ID01 al ID12 incorporando comandos

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -1,39 +1,121 @@
 describe("Test Shady Meadows", () => {
-  beforeEach(() => {
-    cy.IngresarPagina();
-  });
+    beforeEach(() => {
+        // Caso de Prueba ID01 y 02
+        cy.visit('https://automationintesting.online/')
+    })
 
-  //Item 18
-  //   it("login with invalid credentials", () => {
-  //     cy.get(".nav-link").eq(5).click();
-  //     cy.login("admin", "password");
-  //   });
+    // Este condicional lo hice con ayuda de IA porque tenía error de React que rompía el test.
+    Cypress.on('uncaught:exception', (err) => {
+        if (err.message.includes('Minified React error #418')) {
+            return false; // De esta forma se ignora el fallo de hidratación de React y continúa el test
+        }
+    })
 
-  //Item 19
-  //   it("login with invalid credentials", () => {
-  //     cy.get(".nav-link").eq(5).click();
-  //     cy.ValidarDatosVaciosLogin();
-  //   });
+    // Caso de Prueba ID03
+    it('Seleccionar habitacion y abrir el formulario de reserva', () => {
+        cy.hacerReserva().then(({ checkin, checkout }) => {
+            cy.get('a[href="#booking"]').click()
+            cy.get(`a[href*="/reservation/1?checkin=${checkin}&checkout=${checkout}"]`).click()
+            cy.get('#doReservation').click()
 
-  //Item 15
-  //   it("Acceder a amenities", () => {
-  //     cy.get(".nav-link").eq(2).click();
-  //   });
+        })
+    })
 
-  //Item16
-  //   it("Acceder a contact us", () => {
-  //     cy.get(".nav-link").eq(4).click();
-  //     cy.EnviarDatosValidos(
-  //       "Juan",
-  //       "juan@example.com",
-  //       "12345678909",
-  //       "Consulta",
-  //       "Hola, tengo una consulta sobre las habitaciones disponibles.",
-  //     );
-  //   });
-  //Item 17
-  //   it("Validar campos requeridos", () => {
-  //     cy.get(".nav-link").eq(4).click();
-  //     cy.ValidarCamposObligatorios();
-  //   });
+    // Caso de Prueba ID04 y 05
+    it.skip('Completar el formulario con datos válidos', () => {
+        cy.hacerReserva().then(({ checkin, checkout }) => { // comando declarado para no tener problema con las fechas de reserva
+            cy.get('a[href="#booking"]').click()
+            cy.get(`a[href*="/reservation/1?checkin=${checkin}&checkout=${checkout}"]`).click()
+            cy.get('#doReservation').click()
+
+            //cy.get('[placeholder="Firstname"]').type('Juan')
+            //cy.get('[placeholder="Lastname"]').type('Perez')
+            //cy.get('[placeholder="Email"]').type(`mail${Date.now()}@gmail.com`)
+            //cy.get('[placeholder="Phone"]').type('01164666830')
+            cy.datosForm('Juan', 'Perez', `mail${Date.now()}@gmail.com`, '01164666830')
+            cy.get('button.btn-primary.w-100').click()
+        })
+    })
+
+    // Caso de Prueba ID06
+    it.skip('Intentar enviar el formulario con campos vacios ', () => {
+        cy.hacerReserva().then(({ checkin, checkout }) => {
+            cy.get('a[href="#booking"]').click()
+            cy.get(`a[href*="/reservation/1?checkin=${checkin}&checkout=${checkout}"]`).click()
+            cy.get('#doReservation').click()
+
+            cy.get('button.btn-primary.w-100').click()
+        })
+
+    })
+
+    // Caso de Prueba ID07
+    it.skip('Ingresar email invalido', () => {
+        cy.hacerReserva().then(({ checkin, checkout }) => {
+            cy.get('a[href="#booking"]').click()
+            cy.get(`a[href*="/reservation/2?checkin=${checkin}&checkout=${checkout}"]`).click()
+            cy.get('#doReservation').click()
+
+            cy.datosForm('Juan', 'Perez', `mailgmail.com`, '01164666830')
+            cy.get('button.btn-primary.w-100').click()
+        })
+    })
+
+    // Caso de Prueba ID08
+    it.skip('Seleccionar fecha de salida anterior a entrada', () => {
+        cy.hacerReserva(2, 1).then(({ checkin, checkout }) => {
+            cy.get('a[href="#booking"]').click()
+            cy.get(`a[href*="/reservation/2"]`).click()
+            cy.get('#doReservation').click()
+
+            cy.datosForm('Juan', 'Perez', `mail${Date.now()}@gmail.com`, '01164666830')
+            cy.get('button.btn-primary.w-100').click()
+        })
+    })
+
+    //ID 12- Reservar habitacion "Single" y comprobación en Admin
+    it('Reservar habitacion "Single" y comprobación en Admin', () => {
+            cy.hacerReserva().then(({ checkin, checkout }) => { // comando declarado para no tener problema con las fechas de reserva
+                cy.get('a[href="#booking"]').click()
+                cy.get(`a[href*="/reservation/1?checkin=${checkin}&checkout=${checkout}"]`).click()
+                cy.get('#doReservation').click()
+
+                cy.datosForm('Juan', 'Perez', `mail${Date.now()}@gmail.com`, '01164666830')
+                cy.get('button.btn-primary.w-100').click()
+
+            })
+        })
+        //Item 18
+        //   it("login with invalid credentials", () => {
+        //     cy.get(".nav-link").eq(5).click();
+        //     cy.login("admin", "password");
+        //   });
+
+    //Item 19
+    //   it("login with invalid credentials", () => {
+    //     cy.get(".nav-link").eq(5).click();
+    //     cy.ValidarDatosVaciosLogin();
+    //   });
+
+    //Item 15
+    //   it("Acceder a amenities", () => {
+    //     cy.get(".nav-link").eq(2).click();
+    //   });
+
+    //Item16
+    //   it("Acceder a contact us", () => {
+    //     cy.get(".nav-link").eq(4).click();
+    //     cy.EnviarDatosValidos(
+    //       "Juan",
+    //       "juan@example.com",
+    //       "12345678909",
+    //       "Consulta",
+    //       "Hola, tengo una consulta sobre las habitaciones disponibles.",
+    //     );
+    //   });
+    //Item 17
+    //   it("Validar campos requeridos", () => {
+    //     cy.get(".nav-link").eq(4).click();
+    //     cy.ValidarCamposObligatorios();
+    //   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,40 +1,40 @@
 Cypress.Commands.add("IngresarPagina", () => {
-  cy.visit("https://automationintesting.online");
+    cy.visit("https://automationintesting.online");
 });
 
 Cypress.Commands.add("login", (user, pass) => {
-  cy.get("#username").type(user);
-  cy.get("#password").type(pass);
-  cy.get("#doLogin").click();
+    cy.get("#username").type(user);
+    cy.get("#password").type(pass);
+    cy.get("#doLogin").click();
 });
 
 Cypress.Commands.add("ValidarDatosVaciosLogin", () => {
-  cy.get("#doLogin").click();
-  cy.get(".alert.alert-danger").should("contain.text", "Invalid credentials");
+    cy.get("#doLogin").click();
+    cy.get(".alert.alert-danger").should("contain.text", "Invalid credentials");
 });
 
 Cypress.Commands.add(
-  "EnviarDatosValidos",
-  (name, email, phone, subject, mensaje) => {
-    cy.get("#name").type(name);
-    cy.get("#email").type(email);
-    cy.get("#phone").type(phone);
-    cy.get("#subject").type(subject);
-    cy.get("#description").type(mensaje);
-    cy.get(".btn-primary").contains("Submit").click();
+    "EnviarDatosValidos",
+    (name, email, phone, subject, mensaje) => {
+        cy.get("#name").type(name);
+        cy.get("#email").type(email);
+        cy.get("#phone").type(phone);
+        cy.get("#subject").type(subject);
+        cy.get("#description").type(mensaje);
+        cy.get(".btn-primary").contains("Submit").click();
 
-    cy.get(".col-lg-8 > .card > .card-body").contains(
-      "Thanks for getting in touch",
-    );
-  },
+        cy.get(".col-lg-8 > .card > .card-body").contains(
+            "Thanks for getting in touch",
+        );
+    },
 );
 
 Cypress.Commands.add("ValidarCamposObligatorios", () => {
-  cy.get(".btn-primary").contains("Submit").click();
-  cy.get(".alert")
-    .should("contain", "Phone may not be blank")
-    .and("contain", "Name may not be blank")
-    .and("contain", "Email may not be blank")
-    .and("contain", "Subject may not be blank")
-    .and("contain", "Message may not be blank");
+    cy.get(".btn-primary").contains("Submit").click();
+    cy.get(".alert")
+        .should("contain", "Phone may not be blank")
+        .and("contain", "Name may not be blank")
+        .and("contain", "Email may not be blank")
+        .and("contain", "Subject may not be blank")
+        .and("contain", "Message may not be blank");
 });
